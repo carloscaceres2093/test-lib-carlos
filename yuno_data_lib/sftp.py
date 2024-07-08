1aeaf03615ae8c7b099021998c39d492f363bd65
caceresc2093 <carlos.caceres@y.uno>
2024-07-08 13:48:17 -0500

    Add connection by user pass

--- yuno_data_lib/sftp.py
+++ yuno_data_lib/sftp.py
@@ -4,23 +4,20 @@ import logging
 
 def open_ftp_connection(ftp_host, ftp_user, ftp_key):
     """
-    Opens ftp connection and returns connection object.
+    Opens ftp connection.
+    :return: connection object.
     """
-    pkey = paramiko.RSAKey.from_private_key(io.StringIO(ftp_key))
     client = paramiko.SSHClient()
     client.load_system_host_keys()
     try:
         transport = paramiko.Transport(ftp_host)
     except Exception as err:
-        logging.error("Failed to connect FTP Server!")
+        logging.error('Failed to connect FTP Server!')
         raise SystemExit(err)
     try:
-        transport.connect(username=ftp_user, pkey=pkey)
+        transport.connect(username=ftp_user, password=ftp_key)
     except Exception as err:
-        logging.error("Incorrect username or password!")
+        logging.error('Incorrect username or password!')
         raise SystemExit(err)
-    try:
-        ftp_connection = paramiko.SFTPClient.from_transport(transport)
-    except Exception:
-        logging.error("SFTP connection error!")
+    ftp_connection = paramiko.SFTPClient.from_transport(transport)
     return ftp_connection
