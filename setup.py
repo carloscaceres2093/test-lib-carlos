2fd4327fcb4a37fa4ffe5619a433c66b9d61a829
caceresc2093 <carlos.caceres@y.uno>
2024-07-08 11:46:06 -0500

    Fix name utils

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='python_utils',
+    name='yuno-utils',
     version='0.1.0',
     author='Your Name',
     author_email='your.email@example.com',
     description='A brief description of your module',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
-    url='https://https://github.com/carloscaceres2093/test-lib-carlos.git',
+    url='https://github.com/carloscaceres2093/test-lib-carlos.git',
     packages=find_packages(),
     install_requires=[
         'bcrypt==4.1.3',
